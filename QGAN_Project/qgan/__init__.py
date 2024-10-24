6806635353f9e2e1a7cae792a879ddd870b692f2
Reco <quantascriptor@gmail.com>
2024-10-24 10:59:51 -0400

    Delete QGAN_Project directory
    Signed-off-by: Reco <quantascriptor@gmail.com>

--- QGAN_Project/qgan/__init__.py
+++
@@ -1,6 +0,0 @@
-# Quantum Generative Adversarial Network (QGAN)
-# Developed by Reece Colton Dixon
-# License: Commercial Use License and Public Non-Commercial License
-# For license information, see the LICENSE file
-
-# Initialize the QGAN module
